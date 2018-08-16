915efe45d82f78bb3b245966f833e42500c7d1d4
Gengar003 <witt.austin@gmail.com>
2018-08-16 18:30:46 +0000

    changed fingerprint to keygrip in gpg-preset-passphrase.

--- docker/files/usr/local/bin/entrypoint.sh
+++ docker/files/usr/local/bin/entrypoint.sh
@@ -25,17 +25,17 @@ ps aux
 
 # first, get the key's fingerprint.
 KEY_FINGERPRINT=$(gpg2 --fingerprint --fingerprint $KEY_ID | grep -Po "Key fingerprint = (.*)" | head -1 | sed 's/Key fingerprint = //' | sed 's/ *//g')
+KEY_GRIP=$(gpg2 --fingerprint --with-keygrip ${KEY_ID} | grep -Po "Keygrip = (.*)" | head -1 | sed 's/Keygrip = //' | sed 's/ *//g')
 ps aux
 
 # then, load up the passphrase:
 /usr/lib/gnupg2/gpg-preset-passphrase \
 	--preset \
 	--passphrase "${KEY_PASSPHRASE}" \
-		"${KEY_FINGERPRINT}"
+		"${KEY_GRIP}"
 ps aux
 
 # Show the gpg-agent's cache for this key:
-KEY_GRIP=$(gpg2 --fingerprint --with-keygrip ${KEY_ID} | grep -Po "Keygrip = (.*)" | head -1 | sed 's/Keygrip = //' | sed 's/ *//g')
 echo "KEYINFO --no-ask ${KEY_GRIP} Err Pmt Des" | gpg-connect-agent
 ps aux
 
